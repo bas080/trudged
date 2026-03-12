9bed78b5119298cec5ce3b9f9a793833c2bc10f0
bas080 <bassimhuis@gmail.com>
2026-03-12 21:30:12 +0200

    Rename minetest to core in code

--- init.lua
+++ init.lua
@@ -5,22 +5,22 @@ local TRUDGE_THRESHOLD = 2 -- steps before changing the node
 local function track_trudge(pos, _prev_pos, _player)
     pos = vector.add(pos, {x=0, y=-0.1, z=0})
 
-    local node = minetest.get_node(pos)
-    local def = minetest.registered_nodes[node.name]
+    local node = core.get_node(pos)
+    local def = core.registered_nodes[node.name]
 
-    if not def or minetest.get_item_group(node.name, "soil") == 0 then
+    if not def or core.get_item_group(node.name, "soil") == 0 then
         return
     end
 
     -- track trudge count on the node itself
-    local meta = minetest.get_meta(pos)
+    local meta = core.get_meta(pos)
     local count = meta:get_int("trudge_count") + 1
 
     if count >= TRUDGE_THRESHOLD then
-        minetest.set_node(pos, {name = "default:dirt"})
+        core.set_node(pos, {name = "default:dirt"})
         pos.y = pos.y + 1
-        if minetest.registered_nodes[minetest.get_node(pos).name].buildable_to then
-            minetest.remove_node(pos)
+        if core.registered_nodes[core.get_node(pos).name].buildable_to then
+            core.remove_node(pos)
         end
 
         return
