b3562ddf7a338c45fc0862f98d321cf074c11897
bas080 <bassimhuis@gmail.com>
2026-03-18 16:32:17 +0200

    Fix luacheck minor issues

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-local register_on_player_walk = luanti_utils.dofile('register_on_player_walk.lua')
+local register_on_player_walk = luanti_utils.dofile("register_on_player_walk.lua")
 
 local TRUDGE_THRESHOLD = 2 -- steps before changing the node
 
 local function track_trudge(pos, _prev_pos, _player)
-    pos = vector.add(pos, {x=0, y=-0.1, z=0})
+    pos = vector.add(pos, { x = 0, y = -0.1, z = 0 })
 
     local node = core.get_node(pos)
     local def = core.registered_nodes[node.name]
@@ -17,7 +17,7 @@ local function track_trudge(pos, _prev_pos, _player)
     local count = meta:get_int("trudge_count") + 1
 
     if count >= TRUDGE_THRESHOLD then
-        core.set_node(pos, {name = "default:dirt"})
+        core.set_node(pos, { name = "default:dirt" })
         pos.y = pos.y + 1
         if core.registered_nodes[core.get_node(pos).name].buildable_to then
             core.remove_node(pos)
